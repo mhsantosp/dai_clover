99f9418d58b46b205775ffa678ab2318a9202f9f
Maria H <Santos P>
2020-04-10 17:50:47 -0500

    Se corrige el menu hamburguesa.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './Header.scss';
-import {NavLink, withRouter}  from 'react-router-dom'
+import {NavLink, withRouter} from 'react-router-dom'
 
 class Header extends React.Component {
   getNavLinkClass = (path) => {
     return this.props.location.pathname === path ? 'active' : '';
   }
+  menuCollase = () => {
+    window.$('#navbarNav').collapse('hide');
+  }
+
   render() {
     return (
       <header className="header">
-        <nav className="navbar fixed-top navbar-expand-lg navbar-light">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
             <NavLink to="/dai_clover/" className="navbar-brand" id="logo" ></NavLink>
 
@@ -21,16 +25,15 @@ class Header extends React.Component {
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
               <ul className="navbar-nav">
                 <li className={this.getNavLinkClass("/dai_clover/"), "nav-item"}>
-                  <NavLink to="/dai_clover/" className="nav-link" >
-                    <i className="fas fa-home"></i>&nbsp;Inicio
-                    <span className="sr-only">(current)</span>
+                  <NavLink to="/dai_clover/" className="nav-link" id="navLink-1" onClick={this.menuCollase} >
+                    <i className="fas fa-home"></i>&nbsp;Inicio <span className="sr-only">(current)</span>
                   </NavLink>
                 </li>
                 <li className={this.getNavLinkClass("/dai_clover/Sobre-Mi"), "nav-item"}>
-                  <NavLink to="/dai_clover/Sobre-Mi" className="nav-link" >Sobre Mí</NavLink>
+                  <NavLink to="/dai_clover/Sobre-Mi" className="nav-link" id="navLink-2" onClick={this.menuCollase} >Sobre Mí</NavLink>
                 </li>
                 <li className={this.getNavLinkClass("/dai_clover/Portafolio"), "nav-item"}>
-                  <NavLink to="/dai_clover/Portafolio" className="nav-link" >Portafolio</NavLink>
+                  <NavLink to="/dai_clover/Portafolio" className="nav-link" id="navLink-3" onClick={this.menuCollase} >Portafolio</NavLink>
                 </li>
               </ul>
             </div>
--- src/components/sections/home/Inicio.js
+++ src/components/sections/home/Inicio.js
@@ -9,15 +9,16 @@ class Home extends Component {
       <div className="container py-5">
         <h1 className="d-none"> Dai Clover - Maria Helena Santos Pineda - Portafolio</h1>
         <div className="row justify-content-around">
-          <div className="col-sm-12 col-md-4">
+          <div className="col-sm-12 col-md-4 d-flex align-items-center">
             <div className="container py-3 perfil">
               <img src={logotipo} id="logo_marcaPnal" />
             </div>
           </div>
-          <div className="col-sm-12 col-md-7 offset-md-1 d-flex align-items-around">
+          <div className="col-md-1"></div>
+          <div className="col-sm-12 col-md-7 d-flex align-items-around">
             <div className="container my-auto perfil">
               <div className="photo"></div>
-              <p>¡Hola! Soy <strong>Maria Helena</strong> <br/> <b>Desarrolladora Front End</b>. <br /> Me enfoco en la Maquetación, Diseño, Desarrollo y Administración de páginas web. <br/> Como persona, he sido <b>creativa y soñadora</b> toda mi vida. Gracias a que vengo de una familia humilde y amorosa, donde me inculcaron buenos principios y valores, sobre todo la ética y lealtad hacia mí y los que deciden brindarme su confían.</p>
+              <p>¡Hola! Soy <strong>Maria Helena</strong> <br/> <b>Desarrolladora Front End</b>. <br /> Me enfoco en la Maquetación, Diseño, Desarrollo y Administración de páginas web. <br/> Como persona, he sido <b>creativa y soñadora</b> toda mi vida. Gracias a que vengo de una familia humilde y amorosa, donde me inculcaron buenos principios y valores, sobre todo la ética y lealtad hacia mí y los que deciden brindarme su confianza.</p>
               <div className="container redes">
                 <span>
                   <a className="btn-social" href="https://www.linkedin.com/in/maria-helena-santos-pineda-3a952a48/" target="_blank"><i className="fab fa-linkedin-in"></i></a>

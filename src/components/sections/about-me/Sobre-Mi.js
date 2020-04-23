d9310d6e5941c40bd0d82acb44fc690af9e90629
Maria H <Santos P>
2020-04-23 01:30:44 -0500

    Mejoras Home y About Me, Contenido y estilos.

--- src/components/sections/about-me/Sobre-Mi.js
+++ src/components/sections/about-me/Sobre-Mi.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react';
 import './AboutMe.scss';
-import DipIngSist from '../../../images/certificados/Diploma_Ingeniera_de_Sistemas.jpg';
-import DipEsTecnPSw from '../../../images/certificados/Especialización_Tecnológica_en_Pruebas_de_Software.jpg';
-import CurSass from '../../../images/certificados/platzi/Diploma_Curso_de_SASS.jpg';
-import DisPagWebHtmlJs from '../../../images/certificados/sena/Diseñar_Páginas_Web_Html_Javascrip.jpg';
-import DlloWebSassCoffEmmet from '../../../images/certificados/otros/Certificado_Finalizacion_DlloWeb_SassCoffeescriptEmmet.jpg';
-import EafitTestSw from '../../../images/certificados/otros/Diploma_EAFIT_Testing_de_Software.jpg';
-import MarketingDigt from '../../../images/certificados/otros/Diplomado_Marketing_Digital.png';
-import GestiGerencPytos from '../../../images/certificados/otros/Diplomado_Gestión_Gerencia_Proyectos.png';
 
 class AboutMe extends Component {
   render() {
@@ -17,91 +9,52 @@ class AboutMe extends Component {
         <div className="row justify-content-around">
           <div className="col-sm-12 col-md-6 h-100 education">
             <div className="container my-3">
-              <h3><i className="fas fa-award"></i>&nbsp;Certificaciones</h3>
+              <h3><i className="fas fa-award"></i>&nbsp;Educación</h3>
               <div className="container">
+
                 <div className="accordion" id="accordionExample">
                   <div className="card">
                     <div className="card-header" id="headingOne">
                       <h4 className="mb-0">
-                        <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">Ingeniería de Sistemas</button>
+                        <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                          <i className="fas fa-university"></i>&nbsp;Educ. Presencial
+                        </button>
                       </h4>
                     </div>
                     <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
                       <div className="card-body">
-                        <figure className="figure">
-                          <img src={DipIngSist} className="figure-img img-fluid rounded" alt="Diploma Ingeniera de Sistemas" />
-                        </figure>
+                        <h4 className="study">Diseñar Páginas Web con HTML y JavaScript</h4>
+                        <p className="prom">SENA, dic.2019</p>
+                        <h4 className="study">Especialización Tecnológica en Pruebas de Software</h4>
+                        <p className="prom">SENA, Medellín sep.2018</p>
+                        <h4 className="study">Testing de Software</h4>
+                        <p className="prom">EAFIT, Medellín sep.2017</p>
+                        <h4 className="study">Ingeniería de Sistemas</h4>
+                        <p className="prom">Universidad Cooperativa de Colombia, Medellín mar.2017</p>
+                        <h4 className="study">Diplomado Marketing Digital</h4>
+                        <p className="prom">Comfenalco Antioquia, nov.2016</p>
+                        <h4 className="study">Diplomado Gestión y Gerencia de Proyectos</h4>
+                        <p className="prom">Comfenalco Antioquia, jul.2016</p>
                       </div>
                     </div>
                   </div>
+                  
                   <div className="card">
                     <div className="card-header" id="headingTwo">
                       <h4 className="mb-0">
-                        <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Especialización Tecnológica en Pruebas de Software</button>
+                        <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                          <i className="fas fa-glasses"></i>&nbsp;Educ. Virtual
+                        </button>
                       </h4>
                     </div>
                     <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                       <div className="card-body">
-                        <figure className="figure">
-                          <img src={DipEsTecnPSw} className="figure-img img-fluid rounded" alt="Especialización Tecnológica en Pruebas de Softwar" />
-                        </figure>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="card">
-                    <div className="card-header" id="headingThree">
-                      <h4 className="mb-0">
-                        <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Platzi</button>
-                      </h4>
-                    </div>
-                    <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                      <div className="card-body">
-                        <figure className="figure">
-                          <img src={CurSass} className="figure-img img-fluid rounded" alt="Curso de Sass" />
-                        </figure>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="card">
-                    <div className="row p-0 m-0">
-                      <div className="col-6 p-0">
-                        <div className="card-header" id="headingThree">
-                          <h4 className="mb-0">
-                            <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">SENA</button>
-                          </h4>
-                        </div>
-                        <div id="collapseFour" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                          <div className="card card-body">
-                            <figure className="figure">
-                              <img src={DisPagWebHtmlJs} className="figure-img img-fluid rounded" alt="Diseñar Páginas Web Con Html Y Javascrip" />
-                            </figure>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-6 p-0">
-                        <div className="card-header" id="headingThree">
-                          <h4 className="mb-0">
-                            <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">Otros</button>
-                          </h4>
-                        </div>
-                        <div id="collapseFive" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                          <div className="card card-body">
-                            <figure className="figure">
-                              <a className="" href="http://www.linkedin.com/learning/desarrollo-web-con-sass-coffeescript-y-emmet" target="_blank">
-                                <img src={DlloWebSassCoffEmmet} className="figure-img img-fluid rounded" alt="Certificado de Finalizacion Desarrollo Web Con Sass Coffeescript Y Emmet" />
-                              </a>
-                            </figure>
-                            <figure className="figure">
-                              <img src={EafitTestSw} className="figure-img img-fluid rounded" alt="Diploma EAFIT Curso Testing de Software" />
-                            </figure>
-                            <figure className="figure">
-                              <img src={MarketingDigt} className="figure-img img-fluid rounded" alt="Diplomado en Marketing Digital" />
-                            </figure>
-                            <figure className="figure">
-                              <img src={GestiGerencPytos} className="figure-img img-fluid rounded" alt="Diplomado en Gestión y Gerencia de Proyectos" />
-                            </figure>
-                          </div>
-                        </div>
+                        <h4 className="study">
+                          <a href="http://www.linkedin.com/learning/desarrollo-web-con-sass-coffeescript-y-emmet" target="_blank">Desarrollo Web con Sass, Coffeescript y Emmet</a>
+                        </h4>
+                        <p className="prom">Linkedin Learning, feb.2020</p>
+                        <h4 className="study">Curso de SASS</h4>
+                        <p className="prom">Platzi, nov.2018</p>
                       </div>
                     </div>
                   </div>
@@ -109,7 +62,8 @@ class AboutMe extends Component {
               </div>
             </div>
           </div>
-          <div className="col-sm-12 col-md-5 h-100 offset-md-1 skills">
+          
+          <div className="col-sm-12 col-md-6 h-100 skills">
             <div className="container my-3">
               <h3 className="card-title"><i className="fas fa-laptop-code"></i>&nbsp;Habilidades</h3>
               <div className="container cardWrapper p-0">
@@ -137,7 +91,7 @@ class AboutMe extends Component {
                     <div className="back d-flex align-items-center flex-column h-100 mb-3">
                       <div className="card-body">
                         <div className="container-fluid">
-                          <ul className="list-group list-group-flush w-100 text-right">
+                          <ul className="list-group list-group-flush w-100">
                             <li className="list-group-item">JavaScript</li>
                             <li className="list-group-item">Node.js</li>
                             <li className="list-group-item">NPM (Gestión de paquetes Node.js)</li>
--- src/components/sections/home/Inicio.js
+++ src/components/sections/home/Inicio.js
@@ -17,7 +17,7 @@ class Home extends Component {
           <div className="col-12 col-sm-12 col-md-7 d-flex align-items-around">
             <div className="container my-auto perfil">
               <div className="photo"></div>
-              <p>¡Hola! Soy <strong>Maria Helena</strong> <br/> <b>Desarrolladora Front End</b>. <br /> Me enfoco en la Maquetación, Diseño, Desarrollo y Administración de páginas web. <br/> Como persona, he sido <b>creativa y soñadora</b> toda mi vida. Gracias a que vengo de una familia humilde y amorosa, donde me inculcaron buenos principios y valores, sobre todo la ética y lealtad hacia mí y los que deciden brindarme su confianza.</p>
+              <p>¡Hola! Soy <strong>Maria Helena</strong> <br/> <b>Desarrolladora Front End</b>. <br /> Me apasiona la Maquetación, el Diseño, el Desarrollo y Administración de páginas web. <br/> Como persona, he sido <b>creativa y soñadora</b> toda mi vida. Gracias a que vengo de una familia humilde y amorosa, donde me inculcaron buenos <b>principios, valores y respeto,</b> sobre todo la <b>ética y lealtad</b> hacia mí y los que deciden brindarme su confianza.</p>
               <div className="container redes">
                 <span>
                   <a className="btn-social" href="https://www.linkedin.com/in/maria-helena-santos-pineda-3a952a48/" target="_blank"><i className="fab fa-linkedin-in"></i></a>
